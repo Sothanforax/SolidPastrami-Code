282f3149650a4f143c961814af35b55af9faaa45
Sothanforax <39886116+Sothanforax@users.noreply.github.com>
2019-01-05 14:06:25 -0700

    Add files via upload

--- SOURCES/BSREV3A.h
+++ SOURCES/BSREV3A.h
@@ -1,15 +1,60 @@
-#ifndef BSREV3A__H_
-#define BSREV3A__H_
-typedef enum
+#pragma systemFile
+typedef enum lcdbuttons
 {
 	lml =1,
 	lmr =4,
 	lmc =2,
-
 }
-class lcmen
+/*HAM FUNCTIONS CONTAINED BELOW, BE CAREFUL WITH MODIFICATIONS*/
+#ifndef _HAMINIT_H_GUARD
+#define _HAMINIT_H_GUARD
+void haminit()
 {
-	public:
-
-
+	bLCDBacklight=true;
+	displayLCDString(0,0,"Ham Init v0.0.2");
+	displayLCDString(1,0,"BNSLib by JMarple v0.15.0");
+	wait1Msec(900);
+	clearLCDLine(0);
+	clearLCDLine(1);
 }
+#endif
+#ifndef _BATLEV_H_GUARD
+#define _BATLEV_H_GUARD
+void batlev(bool imdt)
+{
+	if(imdt==false)
+	{
+		displayLCDNumber(1,1,nAvgBatteryLevel);
+	}
+	else
+	{
+		displayLCDNumber(1,1,nImmediateBatteryLevel);
+	}
+}
+#endif
+#ifndef _CLRLCD_H_GUARD
+#define _CLRLCD_H_GUARD
+void clrlcd()
+{
+	clearLCDLine(0);
+	clearLCDLine(1);
+}
+#endif
+#ifndef _RSTATE_H_GUARD
+#define _RSTATE_H_GUARD
+void rstate()
+{
+	if (bVEXNETActive == true)
+	{
+		displayLCDCenteredString(0,"vexnet:active");
+	}
+	else
+	{
+		displayLCDCenteredString(0,"vexnet:inactive");
+	}
+	if (bIfiRobotDisabled == true)
+	{
+		displayLCDCenteredString(1,"robot disabled");
+	}
+}
+#endif
